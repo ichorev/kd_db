1ca5891c8fb3566adce8d192a5b8401956502740
Ittai work-laptop <user@server>
2012-09-08 01:23:14 +0300

    first update

---
+++ kd_db/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'kd_db.views.home', name='home'),
+    # url(r'^kd_db/', include('kd_db.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
